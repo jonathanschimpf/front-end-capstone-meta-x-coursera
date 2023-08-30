3321cb490985d4f76c4c016b8595ae8ab7f17221
Jawnathan <jonathan@jonathanschimpf.com>
2023-08-30 17:10:45 -0400

    fixing that Footer.js navLinks shitshow! Pure Javascript is always theanswer. mixing multiple npm installs always can get messy.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   faFacebook,
   faInstagram,
@@ -9,66 +10,78 @@ import {
   faPhone
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
 import './Footer.css';
 import logoWhiteImage from './assets/logo-white.png';
 import pages from '../../utils/pages';
 
 const contacts = [
-  { icon: faLocationDot, info: '17 W 35th St, Chicago, IL 60616', },
-  { icon: faPhone, info: '(312) 599-1234', },
-  { icon: faEnvelope, info: 'imhungry@littlelemon.com', },
+  { icon: faLocationDot, info: '17 W 35th St, Chicago, IL 60616' },
+  { icon: faPhone, info: '(312) 599-1234' },
+  { icon: faEnvelope, info: 'imhungry@littlelemon.com' },
 ];
 
 const socials = [
-  { icon: faFacebook, name: 'facebook', },
-  { icon: faInstagram, name: 'instagram', },
-  { icon: faYoutube, name: 'youtube', },
+  { icon: faFacebook, name: 'facebook' },
+  { icon: faInstagram, name: 'instagram' },
+  { icon: faYoutube, name: 'youtube' },
 ];
 
 const navLinks = Array.from(pages.values()).filter(page => page.anchorable);
 
 const Footer = () => {
+  const handleNavClick = (e) => {
+    e.preventDefault();
+
+    const target = e.currentTarget.getAttribute('href');
+    window.location.href = target;
+  };
+
   return (
     <footer className="site-footer">
       <div className="container grid">
-        <img 
-          className="site-footer-logo" 
-          src={logoWhiteImage} 
-          alt="Little Lemon Logo" 
+        <img
+          className="site-footer-logo"
+          src={logoWhiteImage}
+          alt="Little Lemon Logo"
         />
         <nav className="site-footer-nav">
           <h4>Sitemap</h4>
           <ul>
-            {navLinks.map((navLink, index) => 
+            {navLinks.map((navLink, index) => (
               <li key={index}>
-                <Link to={navLink.path}>{navLink.name}</Link>
+                <a
+                  href={navLink.name === 'Home' ? '/' : navLink.path}
+                  onClick={handleNavClick}
+                >
+                  {navLink.name}
+                </a>
               </li>
-            )}
+            ))}
           </ul>
         </nav>
         <div className="site-footer-contact">
           <h4>Contact us</h4>
           <address>
-          {contacts.map((contact, index) => 
-            <p key={index}>
-              <FontAwesomeIcon className="yellow" icon={contact.icon} /> {contact.info}
-            </p>
-          )}
+            {contacts.map((contact, index) => (
+              <p key={index}>
+                <FontAwesomeIcon className="yellow" icon={contact.icon} />{' '}
+                {contact.info}
+              </p>
+            ))}
           </address>
         </div>
         <div className="site-footer-social">
           <h4>Socials</h4>
-          {socials.map((social, index) => 
+          {socials.map((social, index) => (
             <a
-              key={index} 
-              href={`https://www.${social.name}.com`} 
-              target="_blank" 
-              rel="noreferrer" 
+              key={index}
+              href={`https://www.${social.name}.com`}
+              target="_blank"
+              rel="noreferrer"
             >
               <FontAwesomeIcon icon={social.icon} size="lg" />
             </a>
-          )}
+          ))}
         </div>
       </div>
     </footer>
